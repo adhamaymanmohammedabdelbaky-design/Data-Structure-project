032d27b8b1b90a73ed0510ca46b988228af89ccb
Khaled <khaled.al3zomy@gmail.com>
2026-04-09 16:09:25 +0200

    Test sync with Adham

--- main file.cpp
+++ main file.cpp
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
 #include <iostream>
 using namespace std; 
-=======
 //This is the main main file of the project
->>>>>>> 8d9eedf0c9a092abaff2b6ddf852ce1de7e4d716
+//Synchronization test (khaled)
